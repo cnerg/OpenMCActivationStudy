e7b01f7865a120b613827f97749e8d81d0c5cec8
Anupama Rajendra <113371601+anu1217@users.noreply.github.com>
2024-06-17 16:37:52 -0500

    Removed header entry in Vit_J and Flux_Data

--- VitaminJProcessing.py
+++ VitaminJProcessing.py
@@ -13,15 +13,14 @@ import openmc
 # restructure it into a form appropriate for ALARA.
 
 #Loading the csv that contains the energy bounds of the Vitamin J structure 
-Vit_J_Groups = pd.read_csv('VitaminJEnergyGroupStructure.csv', header=1)
-ebounds_lh = Vit_J_Groups.iloc[:, 1]
+Vit_J = pd.read_csv('VitaminJEnergyGroupStructure.csv')
+ebounds_lh = Vit_J.iloc[:, 1]
 
 ebounds = sorted(ebounds_lh, reverse=True) #Energy bounds arranged from high energy to low energy
         
 #Loading the csv file that contains the energy bins and neutron flux values (from OpenMC)
 #This csv file was created in a Python code that reads the h5 output from OpenMC
-Flux = 'Neutron_Flux.csv'
-Flux_Data = pd.read_csv(Flux, header=1)
+Flux_Data = pd.read_csv('Neutron_Flux.csv')
 # Extract both columns from the CSV file
 energy_lh = Flux_Data.iloc[:, 0]
 energy = sorted(energy_lh, reverse=True) #Energy bins arranged from highest to lowest energy
