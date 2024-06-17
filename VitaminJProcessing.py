bd4505835d5bfe6d9c2b60826d1d0d2d6809df1c
Anupama Rajendra <113371601+anu1217@users.noreply.github.com>
2024-06-17 18:25:24 -0500

    Replaced nested for loops with single for loop
    - Initialized flux_sums with np.zeros()
    
    - Added a for-while-if statement to represent the flux summation/iteration over the bin number

--- VitaminJProcessing.py
+++ VitaminJProcessing.py
@@ -30,49 +30,20 @@ flux = sorted(flux_lh, reverse=True) #Fluxes arranged to highest to lowest corre
 #the Vitamin J structure
 
 #Initializing the array of fluxes that are summed up
-flux_sums = [0] * (len(ebounds) - 1)
+flux_sums = np.zeros(175)
+
+results = zip(energy, flux)
 
 #Iterating over each interval of energies in the Vit J structure
 #Since there are 175 energy bins, there are 175 - 1 = 174 intervals
 
-for i in range(len(ebounds) - 1):
-    lower_bound = ebounds[i]
-    upper_bound = ebounds[i + 1]
-    
-    # If the OpenMC energy bin corresponding to the flux is equal to energy level of bin 175,
-    # this 'if statement' will include the flux in the summation of the last interval
-    # (This is done for completeness - it is unlikely that these two energy values will be
-    # exactly equal to each other)
-    
-    if i == len(ebounds) - 2:  # Checking if it's the last interval
-        for j in range(len(energy)):
-            if lower_bound >= energy[j] >= upper_bound:  # Including the upper energy bound
-                flux_sums[i] += flux[j]
-                
-    # For all other energy intervals, the upper bound is not included in the summation            
-    else:
-        for j in range(len(energy)):
-            if lower_bound >= energy[j] > upper_bound:
-                flux_sums[i] += flux[j]
-    
-#Converting list of fluxes to array format    
-flux_sums_array = np.array(flux_sums)
+groups = len(ebounds) - 1
+bin = 0
 
-#Saving flux_sums as a 30x6 tab-delimited file:
-    
-Array_1 = flux_sums_array[:174] #The first 174 elements of the flux array
-Array_2 = flux_sums_array[174] #The 175th element of the array
-    
-flux_sums_shaped = Array_1.reshape(29,6) #Accounts for the first 174 elements
+for energy, flux in results
+        while bin < groups and ebounds[bin + 1] >= energy
+                bin += 1
+        if bin < len(flux_sums):
+                flux_sums[bin] += flux
 
-# Open the file in append mode and write each row of the reshaped array
-with open('Flux_Summation.txt', 'w') as f:
-    for row in flux_sums_shaped:
-        formatted_row = ' '.join([f'{value:.5e}' for value in row])
-        f.write(formatted_row + '\n')
-
-#Adding the 175th element to the array:
-with open('Flux_Summation.txt', 'a') as f:
-    f.write(f'{Array_2:.5e}\n')
-
-#print(flux_sums_shaped)
+print(flux_sums)
