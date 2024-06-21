d30a1367de55e3faeff21052eb9d0b9a5287f1f2
anu1217 <asrajendra@wisc.edu>
2024-06-21 15:52:57 -0500

    Adding OpenMC SS simulation & associated file

---
+++ SS_Activation_OpenMC.py
@@ -0,0 +1,166 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  8 08:16:12 2024
+
+@author: Anupama Rajendra
+"""
+import openmc
+import openmc.deplete
+import numpy as np
+from fractions import Fraction
+import matplotlib.pyplot as plt
+
+openmc.config['chain_file'] = 'chain_endfb71_sfr.xml'
+
+# Create materials & export to XML:
+#Simulating tungsten shell:
+W = openmc.Material(material_id=1, name='W_Shell')
+W.set_density('g/cm3', 19.28)
+W.add_element('W', 1.00)
+materials = openmc.Materials([W])
+materials.export_to_xml()
+
+# Create geometry
+#Spherical shell:
+    
+R_1 = 1000
+S_1= openmc.Sphere(r=R_1) #sphere of radius 1000cm
+inside_sphere_1 = -S_1
+outside_sphere_1 = +S_1
+R_2 = 1005
+S_2 = openmc.Sphere(r=R_2, boundary_type='vacuum')
+inside_sphere_2 = -S_2
+outside_sphere_2 = +S_2
+S_3 = outside_sphere_1 & inside_sphere_2 #filled with tungsten
+
+# Mapping materials to geometry:
+Void = openmc.Cell(fill=None, region = inside_sphere_1)
+Shell = openmc.Cell(fill=W, region=S_3)
+geometry = openmc.Geometry([Void, Shell])
+geometry.export_to_xml()
+
+# Source distribution:
+PointSource = openmc.stats.Point(xyz=(0.0, 0.0, 0.0))
+Prob = openmc.stats.Discrete(14E+06, 1.0)
+
+# Assign simulation settings
+settings = openmc.Settings()
+settings.batches = 10
+settings.inactive = 1
+settings.particles = 10000
+settings.source = openmc.Source(space=PointSource, energy=Prob, strength = 1.0, particle = 'neutron')
+settings.run_mode = 'fixed source'
+settings.export_to_xml()
+
+# Define tallies
+neutron_tally = openmc.Tally(name="Neutron tally")
+neutron_tally.scores = ['flux', 'elastic', 'absorption']
+# Implementing filter for neutron tally through W shell
+cell_filter = openmc.CellFilter([Shell])
+neutron_tally.filters = [cell_filter]
+
+# Creating a tally to get the flux energy spectrum.
+# An energy filter is created to assign to the flux tally.
+e_min_1, e_max_1 = 5e2, 14.001e6
+groups = 500
+energies = np.logspace(np.log10(e_min_1), np.log10(e_max_1), groups + 1)
+energy_filter_flux = openmc.EnergyFilter(energies)
+
+e_min_2, e_max_2 = 5e2, 14.001e6
+groups = 500
+energies_2 = np.logspace(np.log10(e_min_2), np.log10(e_max_2), groups + 1)
+energy_filter_ng = openmc.EnergyFilter(energies_2)
+
+spectrum_tally = openmc.Tally(name="Flux spectrum")
+# Implementing energy and cell filters for flux spectrum tally
+spectrum_tally.filters = [energy_filter_flux, cell_filter]
+spectrum_tally.scores = ['flux']
+
+# Collecting and exporting tallies to .xml
+tallies = openmc.Tallies([neutron_tally, spectrum_tally])
+tallies.export_to_xml()
+
+model = openmc.model.Model(geometry=geometry,settings=settings)
+#Depletion calculation
+W.depletable = True
+W.volume = 1.33333 * np.pi * (R_2**3 - R_1**3) #volume of W wall material
+chain = openmc.deplete.chain.Chain.from_xml("./chain_endfb71_sfr.xml")
+operator = openmc.deplete.CoupledOperator(model,"./chain_endfb71_sfr.xml", normalization_mode='source-rate')
+time_steps = [100, 36000, 86400]
+source_rates = [1E+18, 0, 0]
+integrator = openmc.deplete.PredictorIntegrator(operator=operator, timesteps=time_steps, source_rates=source_rates, timestep_units='s')
+integrator.integrate()
+
+# Depletion results file
+results = openmc.deplete.Results(filename='depletion_results.h5')
+# Obtain U235 concentration as a function of time
+
+time, n_He4 = results.get_atoms('1', 'He4', nuc_units = 'atom/cm3')
+time, n_W179 = results.get_atoms('1', 'W179', nuc_units = 'atom/cm3')
+time, n_W180 = results.get_atoms('1', 'W180', nuc_units = 'atom/cm3')
+time, n_W181 = results.get_atoms('1', 'W181', nuc_units = 'atom/cm3')
+time, n_W182 = results.get_atoms('1', 'W182', nuc_units = 'atom/cm3')
+time, n_W183 = results.get_atoms('1', 'W183', nuc_units = 'atom/cm3')
+time, n_W184 = results.get_atoms('1', 'W184', nuc_units = 'atom/cm3')
+time, n_W185 = results.get_atoms('1', 'W185', nuc_units = 'atom/cm3')
+time, n_W186 = results.get_atoms('1', 'W186', nuc_units = 'atom/cm3')
+time, n_W187 = results.get_atoms('1', 'W187', nuc_units = 'atom/cm3')
+time, n_Re185 = results.get_atoms('1', 'Re185', nuc_units = 'atom/cm3')
+time, n_Re187 = results.get_atoms('1', 'Re187', nuc_units = 'atom/cm3')
+time, n_Os187 = results.get_atoms('1', 'Os187', nuc_units = 'atom/cm3')
+
+# time, n_He4 = results.get_atoms('1', 'He4')
+# time, n_W180 = results.get_atoms('1', 'W180')
+# time, n_W181 = results.get_atoms('1', 'W181')
+# time, n_W182 = results.get_atoms('1', 'W182')
+# time, n_W183 = results.get_atoms('1', 'W183')
+# time, n_W184 = results.get_atoms('1', 'W184')
+# time, n_W185 = results.get_atoms('1', 'W185')
+# time, n_W186 = results.get_atoms('1', 'W186')
+# time, n_W187 = results.get_atoms('1', 'W187')
+# time, n_Re185 = results.get_atoms('1', 'Re185')
+# time, n_Re187 = results.get_atoms('1', 'Re187')
+# time, n_Os187 = results.get_atoms('1', 'Os187')
+
+print(time, n_He4)
+print(time, n_W179)
+print(time, n_W180)
+print(time, n_W181)
+print(time, n_W182)
+print(time, n_W183)
+print(time, n_W184)
+print(time, n_W185)
+print(time, n_W186)
+print(time, n_W187)
+print(time, n_Re185)
+print(time, n_Re187)
+print(time, n_Os187)
+
+# Plotting the arrays with customizations
+plt.plot(time, n_He4, marker='.', linestyle='solid', color='red', label='He4')
+plt.plot(time, n_W179, marker='.', linestyle='solid', color='blue', label="W179")
+plt.plot(time, n_W180, marker='.', linestyle='solid', color='green', label="W180")
+plt.plot(time, n_W181, marker='.', linestyle='solid', color='deepskyblue', label="W181")
+plt.plot(time, n_W182, marker='.', linestyle='solid', color='peru', label="W182")
+plt.plot(time, n_W183, marker='.', linestyle='solid', color='plum', label="W183")
+plt.plot(time, n_W184, marker='.', linestyle='solid', color='crimson', label="W184")
+plt.plot(time, n_W185, marker='.', linestyle='solid', color='rosybrown', label="W185")
+plt.plot(time, n_W186, marker='.', linestyle='solid', color='gray', label="W186")
+plt.plot(time, n_W187, marker='.', linestyle='solid', color='lime', label="W187")
+plt.plot(time, n_Re185, marker='.', linestyle='solid', color='gold', label="Re185")
+plt.plot(time, n_Re187, marker='.', linestyle='solid', color='tomato', label="Re187")
+plt.plot(time, n_Os187, marker='.', linestyle='solid', color='cyan', label="Os187")
+
+# Adding labels and title
+plt.xlabel('Time after shutdown [s]')
+plt.ylabel('Nuclide density [atoms/cm^3]')
+plt.title('Plot of number density vs time after shutdown')
+
+# Adding a legend
+plt.legend()
+
+plt.savefig('Nuclide_density_OpenMC')
+# Display the plot
+plt.show()
+
+#DH = results.get_decay_heat('1', units='W/cm3', by_nuclide=False)
