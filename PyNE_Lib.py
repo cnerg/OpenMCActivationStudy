a21762dfd177adec78388501a077fd6e210219a2
Anupama Rajendra <113371601+anu1217@users.noreply.github.com>
2024-09-16 08:36:21 -0500

    Add files via upload

---
+++ PyNE_Lib.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  2 01:08:06 2024
+
+@author: Anupama Rajendra
+"""
+
+from pyne.material import Material
+from pyne.material_library import MaterialLibrary
+
+W=Material({"W":1})
+W.density=19.3
+Graphite=Material({"C":1})
+Graphite.density=2.62
+
+MatLib = MaterialLibrary()
+MatLib["W"]=W
+MatLib["Graphite"]=Graphite
+MatLib.write_hdf5("PyNE_Lib_WC.h5", h5_overwrite=True)
