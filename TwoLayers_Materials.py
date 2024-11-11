a3db10238dc2f6cf615c3f0325f9e69343be8c4e
Anupama Rajendra <113371601+anu1217@users.noreply.github.com>
2024-11-11 14:08:45 -0600

    Update references to some variables

--- TwoLayers_Materials.py
+++ TwoLayers_Materials.py
@@ -9,13 +9,13 @@ import openmc
 #ALARA Element Library to read density for specified element:
 def alara_element_densities(filepath):  
     with open(""+filepath+"") as ALARA_Lib:
-        libLines = ALARA_lib.readlines()
+        libLines = ALARA_Lib.readlines()
     num_lines = len(libLines)
     density_dict = {}
     line_num = 0
     while line_num < num_lines:
-        element_data = line[line_num].strip().split()
-        element_name = element_data[0].lower
+        element_data = libLines[line_num].strip().split()
+        element_name = element_data[0].lower()
         density_dict[element_name] = float(element_data[3])
         line_num += int(element_data[4]) + 1
     return density_dict
@@ -27,7 +27,7 @@ def make_element(elements, density_dict):
     for element_id, element in enumerate(elements):
         mat = openmc.Material(material_id=element_id+1, name=element)
         mat.add_element(element, 1.00)
-        mat.set_density('g/cm3', density_dict.get(element))
+        mat.set_density('g/cm3', density_dict.get(element.lower()))
         mats.append(mat)
     return mats
 
