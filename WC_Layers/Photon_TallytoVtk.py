2259f7910a55a5d5779f79be85c7af748e4ddfc7
Anupama Rajendra <113371601+anu1217@users.noreply.github.com>
2024-11-14 16:14:10 -0600

    Add functions to read, plot, and save data

--- WC_Layers/Photon_TallytoVtk.py
+++ WC_Layers/Photon_TallytoVtk.py
@@ -7,26 +7,29 @@ Created on Thu Oct  3 23:18:20 2024
 import openmc
 import matplotlib.pyplot as plt
 
-with openmc.StatePoint("statepoint.10.h5") as sp:
-    flux_spectrum = sp.get_tally(id=2) 
-    mesh=sp.meshes[1]
-    # Get the reshaped tally data
-    tally_data_reshaped = flux_spectrum.get_reshaped_data(value='mean')
+def read_statepoint(sp_filename, flux_spectrum_tally_id, mesh_number, summed_axes_energy_filter, energy_filter_index):
+    with openmc.StatePoint(sp_filename) as sp:
+        flux_spectrum = sp.get_tally(id=flux_spectrum_tally_id) 
+        mesh=sp.meshes[mesh_number]
+        # Return tally data condensed into 1 dimension per filter
+        tally_data_reshaped = flux_spectrum.get_reshaped_data(value='mean')
+        flux_sum_energy = tally_data_reshaped.sum(axis=summed_axes_energy_filter)   
+        #Vitamin-J energy filter:
+        e_filter = flux_spectrum.filters[energy_filter_index]
+        #Lower bounds of the energy bins
+        e_filter_lower = e_filter.bins[:, 0]
+        return e_filter_lower, flux_sum_energy, tally_data_reshaped, mesh
 
-    flux_sum_en = tally_data_reshaped.sum(axis=(0,1,3,4,5))   
-    #Vitamin-J energy filter:
-    e_filter = flux_spectrum.filters[2]
-    #Lower bounds of the energy bins
-    e_filter_lower = e_filter.bins[:, 0]
-
+def plot_flux_data(e_filter_lower, flux_sum_energy, figure_filename):
     # Plot flux spectrum
     fix, ax = plt.subplots()
-    ax.loglog(e_filter_lower, flux_sum_en, drawstyle='steps-post')
+    ax.loglog(e_filter_lower, flux_sum_energy, drawstyle='steps-post')
     ax.set_xlabel('Energy [eV]')
     ax.set_ylabel('Flux [photon-cm/source]')
     ax.grid(True, which='both')
-    plt.savefig('Photon_flux_vs_energy.png')
+    plt.savefig(figure_filename)
     plt.show()
-    
-    mesh_data = tally_data_reshaped.sum(axis=(0,2,3,4,5))
-    vtk = mesh.write_data_to_vtk(filename="Photon_Flux.vtk", datasets={"mean":mesh_data.flatten()})
+
+def write_to_vtk(tally_data_reshaped, summed_axes_mesh_filter, vtk_filename, mesh):
+    mesh_data = tally_data_reshaped.sum(axis=summed_axes_mesh_filter)
+    mesh.write_data_to_vtk(filename=vtk_filename, datasets={"mean":mesh_data.flatten()})
