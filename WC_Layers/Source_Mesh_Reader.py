0713d7343dbcb9cf73da739c70c053b7891561b0
Anupama Rajendra <113371601+anu1217@users.noreply.github.com>
2024-11-15 02:44:42 -0600

    Created functions to extract and write relevant data

--- WC_Layers/Source_Mesh_Reader.py
+++ WC_Layers/Source_Mesh_Reader.py
@@ -16,26 +16,40 @@ Files = [File_1, File_2]
 # Number of photon groups
 photon_groups = 24
 
-# Process each file
-for file_index, file in enumerate(Files):
-    # Extract data from the current file
-    tstt = file['tstt']
-    elements = tstt['elements']
-    tet_four = elements['Tet4']
-    tags = tet_four['tags']
-    sd = tags['source_density'][:]
+def extract_source_data(Files):
+    '''
+    Identifies the location of the source density dataset within each mesh file.
+    '''
+    sd_list = []
+    for file in Files:
+        # Extract data from the current file
+        tstt = file['tstt']
+        elements = tstt['elements']
+        tet_four = elements['Tet4']
+        tags = tet_four['tags']
+        sd = tags['source_density'][:]
+        sd_list.append(sd)
+    return sd_list
     
-    # Write source density to a separate file for each mesh
-    with open(f'source_density_{file_index + 1}.txt', 'w') as source:
-        for tet_element in sd:
-            source.write(' '.join(map(str, tet_element)) + '\n')
+def write_source_density(sd_list, sd_filename):
+    '''
+    Writes each of the specified source density datasets to a text file.
+    
+    sd_list: list of source density datasets from h5m files
+    sd_filename: user-provided filename that appears as a prefix for the text files
+    '''
+    for sd_index, source_density in enumerate(sd_list):
+        # Write source density to a separate file for each mesh
+        with open(f'{sd_filename}_{sd_index + 1}.txt', 'w') as source:
+            for tet_element in source_density:
+                source.write(' '.join(map(str, tet_element)) + '\n')
 
-    # Calculate summed (and individual mesh) strengths for each photon group
-    summed_strengths = []
-    strengths_list = []
-    for group in range(photon_groups):
-        total_strengths = np.sum(sd[:, group])  # Sum over all mesh elements
-        summed_strengths.append(total_strengths)
-        strengths = sd[:,group]
-        strengths_list.append(strengths)
+# Calculate summed (and individual mesh) strengths for each photon group
+summed_strengths = []
+strengths_list = []
+for group in range(photon_groups):
+    total_strengths = np.sum(sd[:, group])  # Sum over all mesh elements
+    summed_strengths.append(total_strengths)
+    strengths = sd[:,group]
+    strengths_list.append(strengths)
         
