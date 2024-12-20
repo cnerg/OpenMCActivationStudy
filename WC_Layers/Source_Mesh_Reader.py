2dde9f37ee228860416b80d1a169155496a81a3f
Anupama Rajendra <113371601+anu1217@users.noreply.github.com>
2024-12-20 10:50:54 -0600

    Update Source_Mesh_Reader.py

--- WC_Layers/Source_Mesh_Reader.py
+++ WC_Layers/Source_Mesh_Reader.py
@@ -2,34 +2,32 @@ import h5py
 import numpy as np
 import yaml
 import argparse
-
+    
 def extract_source_data(source_mesh_list, num_elements, photon_groups):
     '''
     Identifies the location of the source density dataset within each mesh file.
     
     input: iterable of .h5m filenames (str), whose files contain photon source information
-    output: numpy array of source density data with rows = # of mesh elements and 
-    columns = # number of photon groups, with one array per source mesh
-    '''
-    
+    output: numpy array of source density data with rows = # of mesh elements and columns = # number of photon groups, with one array per source mesh
+    ''' 
     sd_list = np.ndarray((len(source_mesh_list), num_elements, photon_groups))
     for source_index, source_name in enumerate(source_mesh_list):
          file = h5py.File(source_name, 'r')
          sd_list[source_index,:] = file['tstt']['elements']['Tet4']['tags']['source_density'][:]
-    return sd_list
+    return sd_list  
 
 def save_source_density(sd_list, sd_filename):
     '''
-    Saves source density data as a text file.
-    
-    sd_list: list of source density datasets from h5m files
-    sd_filename: user-provided filename that appears as a prefix for the text files (str)
+    Saves source density data as a separate text file for each source mesh. Each file is of size photon_groups X num_elements.
+    inputs:
+        sd_list: list of source density datasets from h5m files
+        sd_filename: user-provided filename that appears as a prefix for the text files (str)
     '''
     for sd_index, source_density in enumerate(sd_list):
         with open(f'{sd_filename}_{sd_index + 1}.txt', 'w') as source:
             for tet_element in source_density:
                 source.write(' '.join(map(str, tet_element)) + '\n')
-                
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--Mesh_Reader_YAML', default = 'Source_Mesh_Reader_Inputs.yaml', help="Path (str) to YAML file containing required inputs for Source_Mesh_Reader")
