6cc6b12bee086ab9019e8e6cfc163c8d30cee0ee
Anupama Rajendra <113371601+anu1217@users.noreply.github.com>
2024-12-24 22:33:24 -0600

    Break main() function up into smaller functions

--- SphericalShell/OpenMC_Output_Processing/SS_all_PostProcessing.py
+++ SphericalShell/OpenMC_Output_Processing/SS_all_PostProcessing.py
@@ -2,46 +2,51 @@ import openmc
 import openmc.deplete
 import yaml
 import argparse
-from SS_Tally_PostProcessing import extract_tally_values, plot_flux_tally, save_data
+from SS_Tally_PostProcessing import extract_tally_values, plot_flux_spectrum, save_data
 from SS_Depletion_PostProcessing import extract_nuclides, plot_save_data
-from OpenMC_SS_Material import alara_element_densities, make_element
 
 def main() : 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--yaml_postprocess_path', default = 'SS_Post_Processing_YAML.yaml', help="Path to YAML file containing inputs for post-processing (str)")
-    parser.add_argument('--yaml_model_path', default = 'OpenMC_SS_YAML.yaml', help="Path to YAML file containing required inputs to build model (str)")
-    args = parser.parse_args()
-    yaml_postprocess_path = args.yaml_postprocess_path
-    yaml_model_path = args.yaml_model_path
+    def parse_args():
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--yaml_postprocess_path', default = 'SS_Post_Processing_YAML.yaml', help="Path to YAML file containing inputs for post-processing (str)")
+        parser.add_argument('--yaml_model_path', default = 'OpenMC_SS_YAML.yaml', help="Path to YAML file containing required inputs to build model (str)")
+        args = parser.parse_args()
+        return args
     
-    with open(yaml_postprocess_path, 'r') as yaml_pp :
-        pp_inputs = yaml.safe_load(yaml_pp)
-    indices = pp_inputs['indices']
-    filepaths = pp_inputs['filepaths']
-    units = pp_inputs['units']
+    def read_yamls(args):
+        with open(args.yaml_postprocess_path, 'r') as yaml_pp :
+            pp_inputs = yaml.safe_load(yaml_pp)
+        with open(args.yaml_model_path, 'r') as model_file:
+            model_inputs = yaml.safe_load(model_file)
+        return pp_inputs, model_inputs
     
-    with open(yaml_model_path, 'r') as model_file:
-        model_inputs = yaml.safe_load(model_file)
-    elelib_fp = model_inputs['elelib_fp']    
-    element = model_inputs['element']
-    inner_radius = model_inputs['geom_info']['inner_radius']
-    thickness = model_inputs['geom_info']['thickness'] 
+    def post_process_tallies(pp_inputs, model_inputs):
+        filepaths = pp_inputs['filepaths']   
+        units = pp_inputs['units']        
+        indices = pp_inputs['indices']
+        geom_info = model_inputs['geom_info']
+        
+        tallies, tally_array = extract_tally_values(pp_inputs['filepaths']['statepoint_file_path'])
+        
+        flux_tally = tallies[pp_inputs['indices']['flux_tally_id']]
+        flux_tally_values, energy_bins = plot_flux_spectrum(flux_tally, 
+                          pp_inputs['indices']['energy_filter_index']) 
+        
+        tally_averages =  save_data(flux_tally_values, tally_array, 
+                                    model_inputs['geom_info']['inner_radius'], 
+                                    model_inputs['geom_info']['thickness'])
+
+    def post_process_dep(pp_inputs):
+        nuclide_set, materials_object, dep_results, time_steps = extract_nuclides(pp_inputs['filepaths']['dep_file_path'], 
+                          pp_inputs['units']['time_units'], 
+                          pp_inputs['indices']['depletable_mat_index'])  
+        data_plot = plot_save_data(nuclide_set, materials_object, dep_results, time_steps, 
+                                   pp_inputs['units']['nuc_units'])
     
-    # Tally post-processing:
-    etv = extract_tally_values(filepaths['statepoint_file_path'])
-    pft = plot_flux_tally(etv[0], 
-                          indices['flux_tally_id'], 
-                          indices['energy_filter_index']) 
-    aed = alara_element_densities(elelib_fp)  
-    me = make_element(element, aed, inner_radius, thickness)
-    mat_vol = me[1]
-    sd = save_data(pft[0], mat_vol, etv[1])
-    
-    # Depletion post-processing:
-    en = extract_nuclides(filepaths['dep_file_path'], 
-                          units['time_units'], 
-                          indices['depletable_mat_index'])    
-    psd = plot_save_data(en[0], en[1], en[2], en[3], units['nuc_units'])
+    args = parse_args()
+    pp_inputs, model_inputs = read_yamls(args)
+    tally_processor = post_process_tallies(pp_inputs, model_inputs)
+    depletion_processor = post_process_dep(pp_inputs)    
 
 if __name__ == "__main__":
     main()
