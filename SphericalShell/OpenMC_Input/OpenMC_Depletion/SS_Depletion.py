fd19ab120cac6d16aaabc69b0c4bf75566d70185
Anupama Rajendra <113371601+anu1217@users.noreply.github.com>
2024-12-24 23:01:31 -0600

    Update SS_Depletion.py

--- SphericalShell/OpenMC_Input/OpenMC_Depletion/SS_Depletion.py
+++ SphericalShell/OpenMC_Input/OpenMC_Depletion/SS_Depletion.py
@@ -14,7 +14,6 @@ def deplete_ss(chain_file_path, model, inner_radius, thickness, times_post_boc, 
     source_rates = particle_source_rates
     operator = openmc.deplete.CoupledOperator(model, chain_file, normalization_mode = norm_mode)
     integrator = openmc.deplete.PredictorIntegrator(operator, timesteps, source_rates = source_rates, timestep_units = timestep_units)
-    integrator.integrate()
     return integrator
 
 def main():
