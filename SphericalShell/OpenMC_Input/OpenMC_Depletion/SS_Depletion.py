2e921fed8a954b492dfa533e82e1c95cb68a2e0b
Anupama Rajendra <113371601+anu1217@users.noreply.github.com>
2024-12-24 23:01:14 -0600

    Add nested functions inside main()

--- SphericalShell/OpenMC_Input/OpenMC_Depletion/SS_Depletion.py
+++ SphericalShell/OpenMC_Input/OpenMC_Depletion/SS_Depletion.py
@@ -18,27 +18,37 @@ def deplete_ss(chain_file_path, model, inner_radius, thickness, times_post_boc, 
     return integrator
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--yaml_filepath', default = 'OpenMC_SS_YAML.yaml', help="Path to YAML file containing required inputs (str)")
-    args = parser.parse_args()
-    yaml_filepath = args.yaml_filepath
-    with open(yaml_filepath, 'r') as yaml_dep:
-        dep_inputs = yaml.safe_load(yaml_dep)    
-    geom_info = dep_inputs['geom_info']
-    dep_params = dep_inputs['depletion_params']    
-        
-    chain_file_path = dep_params['chain_file']
-    model_file = dep_params['model_file']
-    model = openmc.model.Model.from_model_xml(path=model_file)
-    inner_radius = geom_info['inner_radius']
-    thickness = geom_info['thickness']
-    times_post_boc = dep_params['times_post_boc']
-    particle_source_rates = dep_params['source_rates']
-    norm_mode = dep_params['norm_mode']
-    timestep_units = dep_params['timestep_units'] 
+    def parse_args():
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--yaml_filepath', default = 'OpenMC_SS_YAML.yaml', help="Path to YAML file containing required inputs (str)")
+        args = parser.parse_args()
+        return args
     
-    dss = deplete_ss(chain_file_path, model, inner_radius, thickness, times_post_boc, particle_source_rates, norm_mode, timestep_units)     
-    dss.integrate()
+    def read_yaml(args):   
+        with open(args.yaml_filepath, 'r') as yaml_dep:
+            dep_inputs = yaml.safe_load(yaml_dep)    
+        return dep_inputs
+    
+    def run_depletion(dep_inputs):
+        geom_info = dep_inputs['geom_info']
+        dep_params = dep_inputs['depletion_params']    
+        model_file = dep_params['model_file']
+        model = openmc.model.Model.from_model_xml(path=model_file)
+    
+        integrator = deplete_ss(dep_params['chain_file'],
+                     model, 
+                     geom_info['inner_radius'], 
+                     geom_info['thickness'], 
+                     dep_params['times_post_boc'], 
+                     dep_params['source_rates'],
+                     dep_params['norm_mode'], 
+                     dep_params['timestep_units'])     
+    
+        integrator.integrate()
+        
+    args = parse_args()
+    dep_inputs = read_yaml(args)
+    depletion = run_depletion(dep_inputs)
     
 if __name__ == "__main__":
     main()
