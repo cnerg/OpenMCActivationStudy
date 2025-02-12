f9a7075c06b8cd963da6640167b0110abe588dc6
Anupama Rajendra <113371601+anu1217@users.noreply.github.com>
2025-02-12 11:44:51 -0600

    Separate main()

--- SphericalShell/OpenMC_Output_Processing/SS_all_PostProcessing.py
+++ SphericalShell/OpenMC_Output_Processing/SS_all_PostProcessing.py
@@ -5,47 +5,47 @@ import argparse
 from SS_Tally_PostProcessing import extract_tally_values, plot_flux_spectrum, save_data
 from SS_Depletion_PostProcessing import extract_nuclides, plot_save_data
 
-def main() : 
-    def parse_args():
-        parser = argparse.ArgumentParser()
-        parser.add_argument('--yaml_postprocess_path', default = 'SS_Post_Processing_YAML.yaml', help="Path to YAML file containing inputs for post-processing (str)")
-        parser.add_argument('--yaml_model_path', default = 'OpenMC_SS_YAML.yaml', help="Path to YAML file containing required inputs to build model (str)")
-        args = parser.parse_args()
-        return args
+def parse_args():
+   parser = argparse.ArgumentParser()
+   parser.add_argument('--yaml_postprocess_path', default = 'SS_Post_Processing_YAML.yaml', help="Path to YAML file containing inputs for post-processing (str)")
+   parser.add_argument('--yaml_model_path', default = 'OpenMC_SS_YAML.yaml', help="Path to YAML file containing required inputs to build model (str)")
+   args = parser.parse_args()
+   return args
+
+def read_yamls(args):
+    with open(args.yaml_postprocess_path, 'r') as yaml_pp :
+        pp_inputs = yaml.safe_load(yaml_pp)
+    with open(args.yaml_model_path, 'r') as model_file:
+        model_inputs = yaml.safe_load(model_file)
+    return pp_inputs, model_inputs
     
-    def read_yamls(args):
-        with open(args.yaml_postprocess_path, 'r') as yaml_pp :
-            pp_inputs = yaml.safe_load(yaml_pp)
-        with open(args.yaml_model_path, 'r') as model_file:
-            model_inputs = yaml.safe_load(model_file)
-        return pp_inputs, model_inputs
-    
-    def post_process_tallies(pp_inputs, model_inputs):
-        filepaths = pp_inputs['filepaths']   
-        units = pp_inputs['units']        
-        indices = pp_inputs['indices']
-        geom_info = model_inputs['geom_info']
+def post_process_tallies(pp_inputs, model_inputs):
+    filepaths = pp_inputs['filepaths']   
+    units = pp_inputs['units']        
+    indices = pp_inputs['indices']
+    geom_info = model_inputs['geom_info']
         
-        tallies, tally_array = extract_tally_values(pp_inputs['filepaths']['statepoint_file_path'])
+    tallies, tally_array = extract_tally_values(pp_inputs['filepaths']['statepoint_file_path'])
         
-        flux_tally = tallies[pp_inputs['indices']['flux_tally_id']]
-        flux_tally_values, energy_bins = plot_flux_spectrum(flux_tally, 
-                          pp_inputs['indices']['energy_filter_index']) 
+    flux_tally = tallies[pp_inputs['indices']['flux_tally_id']]
+    flux_tally_values, energy_bins = plot_flux_spectrum(flux_tally, 
+                      pp_inputs['indices']['energy_filter_index']) 
         
-        tally_averages =  save_data(flux_tally_values, tally_array, 
+    tally_averages =  save_data(flux_tally_values, tally_array, 
                                     model_inputs['geom_info']['inner_radius'], 
                                     model_inputs['geom_info']['thickness'])
 
-    def post_process_dep(pp_inputs):
-        nuclide_set, materials_object, dep_results, time_steps = extract_nuclides(pp_inputs['filepaths']['dep_file_path'], 
-                          pp_inputs['units']['time_units'], 
-                          pp_inputs['indices']['depletable_mat_index'])  
-        data_plot = plot_save_data(nuclide_set, materials_object, dep_results, time_steps, 
-                                   pp_inputs['units']['nuc_units'])
-    
+def post_process_dep(pp_inputs):
+    nuclide_set, materials_object, dep_results, time_steps = extract_nuclides(pp_inputs['filepaths']['dep_file_path'], 
+                      pp_inputs['units']['time_units'], 
+                      pp_inputs['indices']['depletable_mat_index'])  
+    data_plot = plot_save_data(nuclide_set, materials_object, dep_results, time_steps, 
+                      pp_inputs['units']['nuc_units'])
+
+def main() : 
     args = parse_args()
     pp_inputs, model_inputs = read_yamls(args)
-    tally_processor = post_process_tallies(pp_inputs, model_inputs)
+    post_process_tallies(pp_inputs, model_inputs)
     post_process_dep(pp_inputs)    
 
 if __name__ == "__main__":
