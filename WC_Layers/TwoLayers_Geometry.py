23221bd3f4e7e64108693d66735e1d6ffd6302d0
Anupama Rajendra <113371601+anu1217@users.noreply.github.com>
2025-02-24 14:04:36 -0600

    Change order of inputs

--- WC_Layers/TwoLayers_Geometry.py
+++ WC_Layers/TwoLayers_Geometry.py
@@ -1,11 +1,11 @@
 import openmc
 
-def make_spherical_shells(layers, inner_radius, outer_boundary_type):    
+def make_spherical_shells(inner radius, layers, outer_boundary_type):    
     '''
     Creates a set of concentric spherical shells, each with its own material & inner/outer radius.
     inputs:
-        layers: iterable of tuples of OpenMC Material object and its respective thickness (float)
         inner_radius: the radius of the innermost spherical shell
+        layers: iterable of tuples of OpenMC Material object and its respective thickness (float)
     '''
     inner_sphere = openmc.Sphere(r = inner_radius)
     cells = [openmc.Cell(fill = None, region = -inner_sphere)]
