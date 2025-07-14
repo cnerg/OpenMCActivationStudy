f7758eff1618ecb41acac9e2486a99f6d3b71165
Anupama Rajendra <113371601+anu1217@users.noreply.github.com>
2025-07-14 11:56:09 -0500

    Update OpenMC_R2S.py
    - Generalize make_settings() to take source as input instead of neutron_source
    - Call make_settings() twice to assign neutron settings and photon settings separately
    - Rearrange some .export_to_model_xml() calls

--- WC_Layers/OpenMC_R2S.py
+++ WC_Layers/OpenMC_R2S.py
@@ -76,12 +76,12 @@ def make_neutron_tallies(mesh_file):
     
     return openmc.Tallies([neutron_flux_tally])
 
-def make_settings(neutron_source, total_batches, inactive_batches, num_particles, run_mode):
+def make_settings(source, total_batches, inactive_batches, num_particles, run_mode):
     sets = openmc.Settings()
     sets.batches = total_batches
     sets.inactive = inactive_batches
     sets.particles = num_particles
-    sets.source = neutron_source
+    sets.source = source
     sets.run_mode = run_mode
     return sets
 
@@ -157,7 +157,7 @@ def extract_source_data(source_mesh_list, num_elements, num_photon_groups):
          sd_list[source_index,:] = file['tstt']['elements']['Tet4']['tags']['source_density'][:]
     return sd_list   
     
-def make_alara_photon_sources(bounds, cells, mesh_file, source_mesh_indices, sd_list, neutron_model):
+def make_alara_photon_sources(bounds, cells, mesh_file, source_mesh_indices, sd_list):
     '''
     Creates a list of OpenMC sources, complete with the relevant space and energy distributions
     
@@ -169,8 +169,6 @@ def make_alara_photon_sources(bounds, cells, mesh_file, source_mesh_indices, sd_
         
     output:
         all_sources: dictionary of OpenMC independent sources
-        unstructured_mesh: OpenMC Unstructured Mesh object
-        photon_model : OpenMC Model object with photon sources
     '''
     all_sources = {}
     unstructured_mesh = openmc.UnstructuredMesh(mesh_file, library='moab')
@@ -181,10 +179,7 @@ def make_alara_photon_sources(bounds, cells, mesh_file, source_mesh_indices, sd_
             energy_dist = openmc.stats.Uniform(a=lower_bound, b=upper_bound)
             source_list.append(openmc.IndependentSource(space=mesh_dist, energy=energy_dist, strength=np.sum(sd_list[source_mesh_index][:, index]), particle='photon', domains=cells))
             all_sources[source_mesh_index] = source_list
-            photon_model = neutron_model
-            photon_model.settings.source = all_sources[source_mesh_index]
-            photon_model.settings.export_to_xml(f'settings_{source_mesh_index}.xml')
-    return photon_model
+    return all_sources
 
 def make_openmc_photon_sources(num_cooling_steps, activation_mats, unstructured_mesh, neutron_model, inputs):
     sd_data = h5py.File(inputs['source_meshes'][0], 'r')['tstt']['elements']['Tet4']['tags']['source_density'][:]
@@ -284,15 +279,17 @@ def create_geometry_obj(materials, inputs):
     return geometry
 
 def create_neutron_model(inputs, materials, geometry):
-    settings_info = inputs['settings_info']
+    neutron_settings_info = inputs['neutron_settings_info']
     neutron_source = make_neutron_source(inputs['particle_energy'])
-    settings = make_settings(neutron_source,
-                    settings_info['total_batches'],
-                    settings_info['inactive_batches'],
-                    settings_info['num_particles'],
-                    settings_info['run_mode'])   
-    neutron_model = openmc.model.Model(geometry = geometry, materials = materials, settings = settings)
-    neutron_model.export_to_model_xml("neutron_model.xml")
+    neutron_settings = make_settings(neutron_source,
+                    neutron_settings_info['total_batches'],
+                    neutron_settings_info['inactive_batches'],
+                    neutron_settings_info['num_particles'],
+                    neutron_settings_info['run_mode'])   
+    neutron_model = openmc.model.Model(geometry = geometry, materials = materials, settings = neutron_settings)
+    neutron_model.export_to_model_xml('neutron_model.xml')
+    neutron_model_sp = neutron_model.run('neutron_model.xml')
+    neutron_model_sp.rename('neutron_model.statepoint.h5')
     return neutron_model
 
 #Convert the output of R2S Step2 to a format suitable for OpenMC photon transport:
@@ -305,17 +302,28 @@ def read_source_mesh(inputs):
     return sd_list
 
 def create_alara_photon_model(inputs, neutron_model, sd_list):
-    cells = list(neutron_model.geometry.get_all_cells().values())
-    photon_model = make_alara_photon_sources(
+    '''
+    photon_model: neutron_model to which this function adds photon-relevant settings
+    '''
+    cells = list(photon_model.geometry.get_all_cells().values())
+    all_sources = make_alara_photon_sources(
         inputs['source_info']['phtn_e_bounds'],
         cells,
         inputs['filename_dict']['mesh_file'],
         inputs['file_indices']['source_mesh_indices'],
-        sd_list,
-        neutron_model
+        sd_list,         
     )
+    photon_settings_info = inputs['photon_settings_info']
+    for source_mesh_index, photon_source in enumerate(list(all_sources.values())):
+        photon_settings = make_settings(photon_source,
+                        photon_settings_info['total_batches'],
+                        photon_settings_info['inactive_batches'],
+                        photon_settings_info['num_particles'],
+                        photon_settings_info['run_mode'])
+        photon_model.settings = photon_settings
+        photon_model.export_to_model_xml('alara_photon_model_{source_mesh_index}.xml')
     return photon_model
-
+    
 def main():        
     args = parse_args()
     inputs = read_yaml(args)
